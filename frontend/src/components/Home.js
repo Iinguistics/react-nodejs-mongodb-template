5c84217b8c1d4bcf435ff29eb7f9da296bf79910
James <Thylinguistics@gmail.com>
2021-03-02 20:25:05 -0800

    ...

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,9 +4,41 @@ import axios from 'axios';
 
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(()=>{
+    const fetchProducts = async()=>{
+        const res = await axios.get('/api/products');
+        setProducts(res.data.products);
+     }
+    fetchProducts();
+  }, []);
+   
+        const renderProducts = ()=>{
+            if(products !== null){
+                return products.map((item)=>{
+                    return (
+                        <div>
+                            <h3>Name: {item.name}</h3>
+                            <h5>Price$: {item.price}</h5>
+                            <h5>Brand: {item.brand}</h5>
+                        </div>
+                    )
+                })
+            }else{
+                return (
+                    <h1>No products</h1>
+                )
+            }
+        }
+
+
     return (
         <div>
-            home
+            <h1>Products</h1>
+            {renderProducts()}
+             
         </div>
     )
 }
